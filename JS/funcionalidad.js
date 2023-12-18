f43a89b97684723a9a285c91623d11843d005e7a
Tomas Danelutti <tdanelutti@amr.org.ar>
2023-12-18 18:43:47 -0300

    agrego los estilos nuevos, modals y labels a los inputs

--- JS/funcionalidad.js
+++ JS/funcionalidad.js
@@ -1,30 +1,49 @@
+StrictMode = true;
 // Secuencias de colores
 var gameSequence = []; // Secuencia de colores del juego
 var playerSequence = []; // Secuencia de colores del jugador
-var nivel; // Nivel actual
-var puntos; // Puntos actuales
-var nombre= '';
+var level; // Nivel actual
+var score; // Puntos actuales
+var namePlayer= '';
+var namePlayerErrorLabel = document.getElementById("namePlayerErrorLabel");
 const colors = ['red', 'green', 'blue', 'yellow']; // Colores disponibles
 
 // Inicia el juego
-function startGame() {
-  nombre = document.getElementById('nombre').value;
-  nombre = nombre.toUpperCase();
+ function startGame() {
+document.getElementById('start').addEventListener('click', function() {
   // Obtiene el nombre del jugador
-  if (nombre.length < 3) {
-    alert("El nombre debe tener al menos 3 letras.");
-    return;
+  var namePlayer = document.getElementById('namePlayer').value;
+  namePlayer = namePlayer.toUpperCase();
+  if (namePlayer < 3) {
+    namePlayerErrorLabel.style.display = "block";
+    document.getElementById('namePlayer').disabled = false;
+  } else {
+    namePlayerErrorLabel.style.display = "none";
+    reset();
+    start();
   }
+});
+   document.getElementById('aceptar').addEventListener('click', function() {
+     document.getElementById('modalError').style.display = 'none';
+   });
+ }
+
+ function reset() {
   // Reinicia las variables
   gameSequence = [];
   playerSequence = [];
-  nivel = 0;
-  puntos = 0;
+  level = 0;
+  score = 0;
+   document.getElementById('score').innerText = `Puntaje: ${score}`;
+   document.getElementById('level').innerText = `Nivel actual: ${level}`;
+}
+
+function start() {
   // Actualiza el puntaje y el nivel
-  document.getElementById('puntos').innerText = `Puntaje: ${puntos}`;
-  document.getElementById('nivel').innerText = `Nivel: ${nivel}`;
+  document.getElementById('score').innerText = `Puntaje: ${score}`;
+  document.getElementById('level').innerText = `Nivel actual: ${level}`;
   document.getElementById('start').disabled = true;
-  document.getElementById('nombre').disabled = true;
+  document.getElementById('namePlayer').disabled = true;
   // Agrega 20 colores aleatorios a la secuencia
   for(let i = 0; i < 20; i++) {
     gameSequence.push(colors[Math.floor(Math.random() * 4)]);
@@ -38,7 +57,7 @@ document.getElementById('start').addEventListener('click', startGame);
 
 // Muestra la secuencia
 function displaySequence() {
-  for(let i = 0; i <= nivel; i++) {
+  for(let i = 0; i <= level; i++) {
     setTimeout(function() {
       flashButton(gameSequence[i]);
     }, i * 1000);
@@ -67,52 +86,69 @@ for(var i = 0; i < buttons.length; i++) {
     playerSequence.push(e.target.id);
     // Comprueba si el jugador ha perdido
     if(playerSequence[playerSequence.length - 1] !== gameSequence[playerSequence.length - 1]) {
-      alert('¡Has perdido! Haz clic en Comenzar para jugar de nuevo.');
+      document.getElementById('mensajeError').textContent = "¡Has perdido! Nivel: " + level + " Puntaje: " + score +" Haz clic en Comenzar para jugar de nuevo.";
+      document.getElementById('modalError').style.display = 'block';
       document.getElementById('start').disabled = false;
-        document.getElementById('nombre').disabled = false;
+      document.getElementById('namePlayer').disabled = false;
+      document.getElementById('namePlayer').value = '';
+      reset();
       return;
     }
-    puntos++;
-    document.getElementById('puntos').innerText = `Puntaje: ${puntos}`;
+    score++;
+    document.getElementById('score').innerText = `Puntaje: ${score}`;
     // Comprueba si el jugador ha ganado
     if(playerSequence.length === gameSequence.length) {
-      alert('¡Felicidades, has ganado! Haz clic en Comenzar para jugar de nuevo.');
+      document.getElementById('mensajeError').textContent = "¡Felicidades, has ganado! Haz clic en Comenzar para jugar de nuevo.";
+      document.getElementById('modalError').style.display = 'block';
       document.getElementById('start').disabled = false;
-        document.getElementById('nombre').disabled = false;
+      document.getElementById('namePlayer').disabled = false;
+      document.getElementById('namePlayer').value = '';
       return;
     }
     // Comprueba si el jugador ha completado el nivel
-    if(playerSequence.length === nivel + 1) {
-      nivel++;
+    if(playerSequence.length === level + 1) {
+      level++;
       playerSequence = [];
       setTimeout(function() {
         displaySequence();
       }, 1000);
     }
     // Actualiza el nivel
-    document.getElementById('nivel').innerText = `Nivel: ${nivel}`;
+    document.getElementById('level').innerText = `Nivel actual: ${level}`;
   });
 }
+
+
 //Funciones para el Formulario
-function validateForm(){
-  var name = document.getElementById('nameForm').value;
+function validateForm() {
+  var nameInput = document.getElementById("nameForm");
+  var nameErrorLabel = document.getElementById("nameErrorLabel");
   var email = document.getElementById('validateEmail').value;
+  var emailErrorLabel = document.getElementById('emailErrorLabel');
   var comentario = document.getElementById('textArea').value;
+  var comentarioErrorLabel = document.getElementById('comentarioErrorLabel');
   var validName = /^[A-Za-z\s]+$/.test(name);
   var emailValid = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(email);
-  var destinatario ='tomasdanelutti12@gmail.com';
+  var destinatario = 'tomasdanelutti12@gmail.com';
   var affair = 'Consulta desde el formulario de la página';
   var mailtoLink = 'mailto:' + destinatario + '?subject=' + encodeURIComponent(affair) + '&body=' + encodeURIComponent(comentario);
 
-  if (!validName){
-    alert('Ingrese un nombre válido');
-    return false;
-  } else if (!emailValid){
-    alert('Ingrese un email válido');
-    return false;
+
+  if (nameInput.value === "") {
+    nameErrorLabel.style.display = "block";
   } else {
-    // Si el formulario es válido, redirigir el navegador hacia el enlace mailto
-    window.location.href = mailtoLink;
-    return true;
+    nameErrorLabel.style.display = "none";
+  }
+
+  if (email === "") {
+    emailErrorLabel.style.display = "block";
+  } else {
+    emailErrorLabel.style.display = "none";
+  }
+
+  if (comentario === "") {
+    comentarioErrorLabel.style.display = "block";
+  } else {
+    comentarioErrorLabel.style.display = "none";
   }
 }
